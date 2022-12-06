acabaa88306e00f511c7f58b57419f44e71d2943
Sebastian Franco <jhoan-sebastian-fran.tilano@kriegerdigital.de>
2022-12-06 16:38:04 +0100

    fix: add pipelines by dockerfile

--- templates/generate-repos.py
+++ templates/generate-repos.py
@@ -1,40 +1,62 @@
-import os, subprocess
+import os
+import subprocess
 import argparse
 import yaml
 
-# TODO: better to use the build scripts as a source of dockerfiles to build and
-# better to create a manifest for each dockerfile so that way I can handle incremental changes instead of build all repos everytime?
-# Define the command-line arguments that the script accepts
+#!/usr/bin/env python
+from subprocess import check_output, STDOUT, CalledProcessError
+
+# default image registry
+REGISTRY_REPO=os.getenv("REGISTRY_REPO", "devops-images")
+
 parser = argparse.ArgumentParser()
 parser.add_argument('--file', help='the name of the file to write', required=True)
+parser.add_argument('--path', help='path where dockerfiles live', required=True)
+parser.add_argument('--dockerfile', help='path of specific dockerfile you want to build', default="")
 args = parser.parse_args()
 
 # Get the path of the script's directory
 script_dir = os.path.dirname(os.path.realpath(__file__))
 root_dir   = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 
+# the script you want to execute
+script = os.path.join(root_dir, 'bin/build')
+dockerfiles = ""
+
+try:
+    # execute the script and save the output -> bin/build
+    dockerfiles = check_output(
+        ["bash", script, REGISTRY_REPO, args.path, args.dockerfile],
+        stderr=STDOUT, 
+        text=True
+    )
+except CalledProcessError as exc:
+    print(script, exc.output)
+    exit
+
+builds = dockerfiles.splitlines()
 
 # Define the pipeline configuration template
 pipeline_template = """
-docker-build-{PROJECT}:
+docker-build-{IMAGE_NAME}:
     extends: .docker-builds
-    script:
-        - bin/build {REGISTRY_REPO} {PATH_PROJECT}
+    variables:
+        CONTEXT: {CONTEXT}
+        DOCKERFILE: {DOCKERFILE}
+        IMAGE_NAME: {IMAGE_NAME}
+        GLOBAL_SCRIPTS: {GLOBAL_SCRIPTS}
 """
-
-# images is the default directory for all docker images
-path        = os.path.join(root_dir, 'images')
-directories = os.listdir(path)
 
 data_pipeline = ""
 
-for directory in directories:
-    path_project = path + "/" + directory
-    # Use string formatting to insert the values of the variables into the template
+for build in builds:
+    [context, dockerfile, image_name, global_scripts] = build.split("*")
+
     data_pipeline += pipeline_template.format(
-        PATH_PROJECT=path_project,
-        PROJECT=directory,
-        REGISTRY_REPO=os.getenv("REGISTRY_REPO", "devops-images") # should live in the CICD
+        CONTEXT=context,
+        DOCKERFILE=dockerfile,
+        IMAGE_NAME=image_name,
+        GLOBAL_SCRIPTS=global_scripts
     )
 
 # Load the configuration as a dictionary
